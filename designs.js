34c65d747b9a03a16d831770ad8e06809b63104d
Amir.E Haji Hussaini <niamileo@gmail.com>
2019-11-11 18:47:01 +0330

    makeGrid Function is done!

--- designs.js
+++ designs.js
@@ -1,10 +1,34 @@
+// Varuables
+const pixelTable = document.querySelector('#pixelCanvas');
+const inputWidth = document.querySelector('#inputWidth');
+const inputHeight = document.querySelector('#inputHeight');
+const submitBtn = document.querySelector('#inputSubmit');
+
+
 // Select color input
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
-// Your code goes here!
-
+function makeGrid(r, c) {
+    // Your code goes here!
+    // console.log(inputWidth.value);
+    console.log(r + " " + c);
+    for (let i = 1; i <= r; i++) {
+        let tableRow = document.createElement("tr");
+        let tr = pixelTable.appendChild(tableRow);
+        // console.log(i);
+        for (let j = 1; j <= c; j++) {
+            let tableCol = document.createElement("td");
+            tr.appendChild(tableCol);
+            // console.log(j);
+        }
+    }
 }
+
+submitBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    let r = inputWidth.value;
+    let c = inputHeight.value;
+    makeGrid(r, c);
+});
